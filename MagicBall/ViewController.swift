c7c5aabfb8d0acc1fc87f1cfa6566fca69e8a6be
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-09 20:58:13 +0300

    logic added

--- MagicBall/ViewController.swift
+++ MagicBall/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ballImageView.image = UIImage(imageLiteralResourceName: "ball3")
     }
 
-
+    @IBAction func askButtonPressed() {
+        let answers = [
+            UIImage(imageLiteralResourceName: "ball1"),
+            UIImage(imageLiteralResourceName: "ball2"),
+            UIImage(imageLiteralResourceName: "ball3"),
+            UIImage(imageLiteralResourceName: "ball4"),
+            UIImage(imageLiteralResourceName: "ball5")
+        ]
+      
+        ballImageView.image = answers.randomElement()
+    }
+    
 }
 
